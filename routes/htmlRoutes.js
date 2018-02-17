926e46d268124666d219b0c7bd79d25986341fbd
Mariam Joan <royalton@gmail.com>
2018-02-17 00:47:46 -0800

    mysql jawsdb

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,4 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 var path = require("path");
-
 
 module.exports = function(app) {
 
@@ -11,7 +6,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/main.html"));
   });
 
-  // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/main.html"));
   });
--- server.js
+++ server.js
@@ -10,18 +10,11 @@ const connection = require("./connection");
 
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 3306;
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
