51a6441bc52af2650518c9fc95a8ec1f32e251b5
Mariam Joan <royalton@gmail.com>
2018-02-17 21:29:57 -0800

    rebase

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,12 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
+  	console.log("this works!")
     res.sendFile(path.join(__dirname, "../public/main.html"));
   });
 
   app.get("*", function(req, res) {
+  	console.log("this works!")
     res.sendFile(path.join(__dirname, "../public/main.html"));
   });
 
